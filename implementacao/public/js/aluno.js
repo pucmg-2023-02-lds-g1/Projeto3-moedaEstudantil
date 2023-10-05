190aaf13cf71a4e8d8204e0f24b3d1294e5dd415
AmandaMS09 <mandanevs@gmail.com>
2023-10-05 15:01:31 -0300

    Visualização dos dados de um aluno #5

--- implementacao/public/js/aluno.js
+++ implementacao/public/js/aluno.js
@@ -1,11 +1,12 @@
 function updateAluno() {
-    let nome, cpf, email, endereco, instituicao, curso, moeda;
+    let nome, cpf, email, endereco, instituicao, curso;
+    // Adicionar os 'var = document.getElementById("").value'
 
     fetch("http://localhost:3000/updateAluno", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            nome, cpf, email, endereco, instituicao, curso, moeda
+            nome, cpf, email, endereco, instituicao, curso
         })
     }).then(function(res) {
         res.json().then(function(data) {
@@ -13,6 +14,29 @@ function updateAluno() {
             if(data.s){
                 window.location.reload();
             }
+        })
+    })
+}
+
+function viewAluno(id) {
+    let nome = "", cpf = "", email = "", endereco = "", instituicao = "", curso = "", moeda = "";
+
+    fetch("http://localhost:3000/viewAluno", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            id
+        })
+    }).then(function(res) {
+        res.json().then(function(data) {
+            if(!data.aluno){
+                window.alert(`${data.tipo} - ${data.mensagem}`)
+                window.location.reload();
+            } else {
+                nome = data.aluno.nome, cpf = data.aluno.cpf, email = data.aluno.email, endereco = data.aluno.endereco, instituicao = data.aluno.instituicao, curso = data.aluno.curso, moeda = data.aluno.moeda;
+            }
+
+            // Adicionar os 'document.getElementById("").value = var'
         })
     })
 }
--- implementacao/server.js
+++ implementacao/server.js
@@ -70,3 +70,22 @@ app.post("/updateAluno", function(req, res){
     })
   })
 })
+
+
+app.post("/viewAluno", function(req, res){
+  connection.query(`SELECT * FROM aluno WHERE id = ${req.body.id};`,
+  (err, rows, fields) => {
+    if(err) {
+      return res.json({
+        tipo: "Erro ao retornar dados do aluno",
+        mensagem: err
+      })
+    }
+    
+    return res.json({
+      aluno: {
+        nome: rows[0].nome, cpf: rows[0].cpf, email: rows[0].email, endereco: rows[0].endereco, instituicao: rows[0].instituicao, curso: rows[0].curso, moeda: rows[0].moeda
+      }
+    })
+  })
+})
