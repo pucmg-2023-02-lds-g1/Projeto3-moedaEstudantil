5231899bd236bceaa3a34aa9805fb6b836905df2
Luizgms <luiz.mendes@sga.pucminas.br>
2023-10-28 13:12:43 -0300

    ExtratoDoALuno

--- implementacao/server.js
+++ implementacao/server.js
@@ -14,13 +14,8 @@ app.use(express.static("public"))
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-<<<<<<< Updated upstream
-    password: "coxinha",
-    database: "moedaestudantil",
-=======
     password: "",
-    database: "mydb",
->>>>>>> Stashed changes
+    database: "moedaestudantil",
 });
 
 //INICIALIZATION CONNECTION WITH DATABASE
@@ -441,4 +436,55 @@ app.post("/viewTransacoesProfessor", function(req, res){
       transacoes: rows
     })
   })
+})
+
+app.post("/pesquisarExtratoAluno", function (req, res) {
+
+connection.query(`SELECT * FROM Transacoes WHERE Aluno_idAluno = ?;`, [req.body.id], (err, rows, fields) => {
+  if (err) {
+    return res.json({
+      tipo: "Erro ao retornar transações do Aluno",
+      mensagem: err
+    });
+  }
+
+  const transacoes = rows;
+
+  if (transacoes.length === 0) {
+    return res.json({
+      transacoes: []
+    });
+  }
+
+
+  const professorIDs = transacoes.map(transacao => transacao.Professor_idProfessor);
+  const professorIDsString = professorIDs.join(',');
+
+
+  connection.query(`SELECT idProfessor, nome FROM Professor WHERE idProfessor IN (${professorIDsString});`, (err, professorRows, fields) => {
+    if (err) {
+      return res.json({
+        tipo: "Erro ao retornar nomes dos professores",
+        mensagem: err
+      });
+    }
+
+    const professorMap = {};
+    professorRows.forEach(professor => {
+      professorMap[professor.idProfessor] = professor.nome;
+    });
+
+    const transacoesComNomes = transacoes.map(transacao => {
+      return {
+        ...transacao,
+        nomeProfessor: professorMap[transacao.Professor_idProfessor]
+      };
+    });
+
+    return res.json({
+      transacoes: transacoesComNomes
+    });
+  });
+});
+
 })
